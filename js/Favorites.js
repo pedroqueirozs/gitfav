609fdf060cabd08c1c768468ab65ec141962626b
pedro <douglas.333.goncalves@gmail.com>
2023-12-28 11:14:43 -0300

    project completion

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,17 +1,26 @@
 import { GitHubUser } from "./GitHubUser.js";
-
 //Classe que ira conter a logica dos dados//
 export class Favorites {
-  //esse root é o app do meu HTML
+  //esse root é o app do meu HTML//
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
-    GitHubUser.search("pedroqueirozs").then((user) => console.log(user));
+    this.checkFavorites();
   }
-
-  //função load para carregamento dos dados //
+  //função load para carregamento dos dados//
+  removeBottomFavorites() {
+    this.hTwo = this.root.querySelector("h2");
+    this.hTwo.classList.toggle("invisible");
+  }
   load() {
-    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
+    this.tbody = this.root.querySelector("table tbody");
+    this.entries =
+      JSON.parse(localStorage.getItem("@github-favorites:")) || [].length;
+  }
+  checkFavorites() {
+    if (this.entries.length === 0) {
+      this.removeBottomFavorites();
+    }
   }
   save() {
     localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
@@ -20,11 +29,11 @@ export class Favorites {
     try {
       const useExists = this.entries.find((entry) => entry.login === username);
       if (useExists) {
-        throw new Error("Usuario já cadastrado");
+        throw new Error("User already registered");
       }
       const user = await GitHubUser.search(username);
       if (user.login === undefined) {
-        throw new Error("Ùsuario não encontrado!");
+        throw new Error("User not found!");
       }
       this.entries = [user, ...this.entries];
       this.update();
@@ -33,39 +42,39 @@ export class Favorites {
       alert(error.message);
     }
   }
-
   delete(user) {
     // neste bloco o filter vai excluir a linha que lhe for passada atraves do retorno falso  //
     const filteredEntries = this.entries.filter(
       (entry) => entry.login != user.login
     );
-    //se retornar falso ele retira do array se for verdadeiro ele coloca no arrray
+    //se retornar falso ele retira do array se for verdadeiro ele coloca no arrray//
     this.entries = filteredEntries;
     this.update();
     this.save();
   }
 }
-//Classe que irá criar a visualização e eventos do HTML
+//Classe que irá criar a visualização e eventos do HTML//
 export class FavoritesView extends Favorites {
   constructor(root) {
-    //criando a conecção entrea as duas classes
+    //criando a conecção entrea as duas classes//
     super(root);
     this.tbody = this.root.querySelector("table tbody");
-
+    this.hTwo = this.root.querySelector("h2");
     this.update();
     this.onAdd();
   }
+
   onAdd() {
-    const addButton = this.root.querySelector(".inputs button");
-    addButton.onclick = () => {
+    const addBottom = this.root.querySelector(".inputs Button");
+    addBottom.onclick = () => {
       const { value } = this.root.querySelector(".inputs input");
+      this.checkFavorites();
       this.add(value);
     };
   }
 
   update() {
     this.removeAllTr();
-
     this.entries.forEach((user) => {
       const row = this.createRow();
 
@@ -83,6 +92,7 @@ export class FavoritesView extends Favorites {
         const isOk = confirm("Tem certeza que deseja deletar essa linha ?");
         if (isOk) {
           this.delete(user);
+          this.checkFavorites();
         }
       };
 
@@ -91,7 +101,7 @@ export class FavoritesView extends Favorites {
   }
 
   createRow() {
-    //criando a parte de contrução da linha com html , O TR tem que ser criado com a DOM. O tr sera o container que vai conter to o conteudo  //
+    //criando a parte de contrução da linha com html, O TR tem que ser criado com a DOM. O tr sera o container que vai conter to o conteudo  //
     const tr = document.createElement("tr");
     //colocar o conteudo dentro do elemnto tr que foi criado pela DOM
     tr.innerHTML = `
@@ -109,11 +119,12 @@ export class FavoritesView extends Favorites {
       <td class="remove">remover</td>
   
       `;
+
     return tr;
   }
-  //Funççao que faz a remoção da linha( o programa ja inicia com ela sendo executada, sendo chamada na função Update)
+  //Funçao que faz a remoção da linha( o programa ja inicia com ela sendo executada, sendo chamada na função Update)
   removeAllTr() {
-    //forEach executara a funcao que lhe for passada para cada um dos trs da tabela
+    //forEach executara a funcao que lhe for passada para cada um dos trs da tabela//
     this.tbody.querySelectorAll("tr").forEach((tr) => {
       tr.remove();
     });
